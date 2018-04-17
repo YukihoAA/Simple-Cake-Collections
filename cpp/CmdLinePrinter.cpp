2f141e998c5cd79e302dc422aa5a7e46e8ff1a45
유키 <yukiho5048@naver.com>
2018-04-17 23:32:37 +0900

    Add Force SW_SHOW

--- cpp/CmdLinePrinter.cpp
+++ cpp/CmdLinePrinter.cpp
@@ -2,14 +2,23 @@
 //
 
 #include <stdio.h>
+#include <Windows.h>
 
 int main(int argc, char* argv[])
 {
+	HWND hWnd = GetConsoleWindow();
+	char CmdLine[MAX_PATH] = "";
+	for (int i = 1; i < argc; i++)
+		strcat(CmdLine, argv[i]);
+	if (hWnd == NULL || ShowWindow(hWnd, SW_SHOW) == 0) {
+		ShellExecuteA(NULL, "open", argv[0], CmdLine, NULL, SW_SHOW);
+	}
+
 	printf("Argument Count: %d\n", argc);
 
 	for (int i = 0; i < argc; i++)
 		printf("argv[%d]: %s\n", i, argv[i]);
 
-	getchar();
+	system("pause");
 	return 0;
 }

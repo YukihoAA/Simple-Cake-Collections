cffd4a72f9e3ab3d36d5ebe558c2abd4743c0016
Yuki <yukiho5048@naver.com>
2018-12-18 16:39:18 +0900

    Force SW_SHOW

--- cpp/CmdLinePrinter_Unicode.cpp
+++ cpp/CmdLinePrinter_Unicode.cpp
@@ -11,6 +11,18 @@ int wmain(void)
 	int argc = 0;
 	LPWSTR *argv = CommandLineToArgvW(GetCommandLine(), &argc);
 	HWND hWnd = GetConsoleWindow();
+	
+	if (hWnd == NULL || ShowWindow(hWnd, SW_SHOW) == 0) {
+		WCHAR CmdLine[MAX_PATH] = L"";
+
+		for (int i = 1; i < argc; i++) {
+			wcscat_s(CmdLine, argv[i]);
+			wcscat_s(CmdLine, L" ");
+		}
+
+		ShellExecuteW(NULL, L"open", argv[0], CmdLine, NULL, SW_SHOW);
+		return 0;
+	}
 
 	//Switch the Console to UTF-16 mode
 	_setmode(_fileno(stdout), _O_U16TEXT);
